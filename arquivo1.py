cb451dbdf90b04845e9aab74a21e6053be9849f3
RodrigoMartinez1110 <jorbinaldo@gmail.com>
2025-04-24 22:38:44 -0300

    Adicionando funcoes matematicas e print com idade somando

--- arquivo1.py
+++ arquivo1.py
@@ -1,4 +1,6 @@
+from funcoes_mat import soma
+
 nome = 'rodrigo'
 sobrenome = 'martinez'
 
-print(f"Meu nome é {nome} e meu sobrenome é {sobrenome}")
+print(f"Meu nome é {nome} e meu sobrenome é {sobrenome} e tenho {soma(1, 23)} anos")
